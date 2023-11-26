ee4de8f21c7c73d2697fb3960e55c82464912796
marunero <j0070ak@kaist.ac.kr>
2023-11-27 00:14:01 +0900

    set UI with .js (only front-end, no back-end)

--- popup.js
+++ popup.js
@@ -1,32 +1,31 @@
-/**
-* ---------------------------------------------------------------------------------
-* | 팝업 |
-* ---------------------------------------------------------------------------------
-**/
 
-// changeColor ID element 를 취득
-let changeColor = document.getElementById("changeColor");
+document.addEventListener('DOMContentLoaded', function() {
+  var levelSlider = document.getElementById('levelSlider');
+  var levelValue = document.getElementById('levelValue');
+  var resolutionValue = document.getElementById('resolutionValue');
+  var ad30sCount = document.getElementById('ad30sCount');
+  var ad5sCount = document.getElementById('ad5sCount');
 
-// 스토리지에 저장되어 있는 컬러가 있다면 표시
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
+  var levels = [
+      { resolution: '480p30', ad30s: 0, ad5s: 0 },
+      { resolution: '720p30', ad30s: 0, ad5s: 1 },
+      { resolution: '720p60', ad30s: 1, ad5s: 0 },
+      { resolution: '1080p30', ad30s: 1, ad5s: 1 },
+      { resolution: '1080p60', ad30s: 2, ad5s: 0 },
+      { resolution: '4K30', ad30s: 3, ad5s: 2 }
+  ];
+
+  function updateUI(level) {
+      levelValue.textContent = level;
+      resolutionValue.textContent = levels[level].resolution;
+      ad30sCount.textContent = levels[level].ad30s;
+      ad5sCount.textContent = levels[level].ad5s;
+  }
+
+  levelSlider.oninput = function() {
+      updateUI(this.value);
+  };
+
+  // 초기 UI 설정
+  updateUI(levelSlider.value);
 });
-
-// 배경색 버튼을 클릭하였을 경우 이벤트 등록
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
-  });
-});
-
-/**
- * @description 현재 웹 페이지의 Body 요소의 배경색을 변경해주는 함수
- **/
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
-}
